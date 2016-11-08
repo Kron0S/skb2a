73aefa5bbed5abe545f158b72ac00a525e62afc5
Kron0S <kron00000s@gmail.com>
2016-11-08 13:37:26 +0500

    output format

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import express from 'express'
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.get('/', async (req, res) => {
 	const a = parseFloat(req.query.a || 0);
 	const b = parseFloat(req.query.b || 0);
 	const sum = a + b;
-	return res.json({
-		sum: sum
-	});
+	return res.send('' + sum);
 });
 
 app.listen(3000, () => {
