1cc4ed4d86449dd0213c5a4381af5457226d1637
Kron0S <kron00000s@gmail.com>
2016-11-08 12:51:28 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express
+app.get('/', async (req, res) => {
+	const a = parseFloat(req.query.a || 0);
+	const b = parseFloat(req.query.b || 0);
+	const sum = a + b;
+	return res.json({
+		sum: sum
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Example app listening on port 3000!');
+});
